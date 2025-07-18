70e49c7d63b47a576bdbc904c8a648d156480993
mk01919 <michaelkini019@gmail.com>
2025-07-18 04:28:10 +0200

    Add files via upload

---
+++ scripts/mains.js
@@ -0,0 +1,11 @@
+function CalculEleve(){
+	let nom=document.getElementById("nom").value;
+	let postnom=document.getElementById("postnom").value;
+	let prenom=document.getElementById("prenom").value;
+	let sexe=document.getElementById("sexe").value;
+}
+	if (true) {
+		document.getElementByClassName("AfficheMessage").innerHTML= "Votre nom est"+nom, postnom, prenom+" et vous etes enregistre";
+	} else(false) {
+		
+	}
